baecb1e45bc40118439cec2d5ad3181a0ddd5510
TochuGV <tobiasvallejos05@gmail.com>
2026-07-06 12:21:31 -0300

    Mejorar el cuerpo de la notificación de la alerta

--- src/main/java/ar/edu/utn/frba/ddsi/climalert/models/entities/notificacion/Notificador.java
+++ src/main/java/ar/edu/utn/frba/ddsi/climalert/models/entities/notificacion/Notificador.java
@@ -1,10 +1,13 @@
 package ar.edu.utn.frba.ddsi.climalert.models.entities.notificacion;
 
 import ar.edu.utn.frba.ddsi.climalert.adapters.CorreoAdapter;
+import ar.edu.utn.frba.ddsi.climalert.models.entities.clima.Clima;
 import ar.edu.utn.frba.ddsi.climalert.models.entities.clima.RegistroClima;
+import ar.edu.utn.frba.ddsi.climalert.models.entities.clima.Ubicacion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
@@ -13,9 +16,30 @@ public class Notificador {
   private final CorreoAdapter adapter;
 
   public void notificarAlerta(RegistroClima registroClima, List<String> destinatarios) {
-    String cuerpo = "¡ALERTA METEOROLÓGICA!\nTemperatura: "
-      + registroClima.getClima().getTemperatura() + "°C\nHumedad: "
-      + registroClima.getClima().getHumedad() + "%";
+    Clima clima = registroClima.getClima();
+    Ubicacion ubicacion = clima.getUbicacion();
+
+    DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss");
+    String fechaFormateada = registroClima.getFecha().format(formatoFecha);
+
+    String cuerpo = """
+                ¡ALERTA METEOROLÓGICA!
+                
+                Se han detectado condiciones climáticas extremas en la siguiente ubicación:
+                📍 %s, %s (%s)
+                
+                Detalles del reporte actual:
+                🌡️ Temperatura: %.1f °C
+                💧 Humedad: %d%%
+                🕒 Fecha del registro: %s
+                """.formatted(
+      ubicacion.nombre(),
+      ubicacion.region(),
+      ubicacion.pais(),
+      clima.getTemperatura(),
+      clima.getHumedad(),
+      fechaFormateada
+    );
 
     for (String destinatario : destinatarios) {
       Notificacion notificacion = new Notificacion(destinatario, cuerpo);
